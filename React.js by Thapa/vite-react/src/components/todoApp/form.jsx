114837c48d9c8df1a11e9bf557ba7380c923cbf6
Shivam Kumar <shivam865889@gmail.com>
2025-08-20 00:21:27 +0530

    compliteon of todo list except set on local storage

--- React.js by Thapa/vite-react/src/components/todoApp/form.jsx
+++ React.js by Thapa/vite-react/src/components/todoApp/form.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
-import {TodoList} from "./todoList"
+import { PiChecksDuotone } from "react-icons/pi";
+import { MdDeleteForever } from "react-icons/md";
 
 export const FormButton = ()=>{
 
@@ -27,13 +28,41 @@ export const FormButton = ()=>{
 
   }
 
+  const handleDeleteBtn=(value)=>{
+     let temp = task.filter((item)=>{
+      if(item.content !== value.content)
+        return item;
+     })
+     setTask(temp);
+  }
+
+  const handleCheckBtn =(value)=>{
+    let temp =task.map((item)=>{
+       if(item.id == value.id)
+        item.checked = !value.checked;
+      return item;
+    })
+    setTask(temp);
+  }
+
     return (
       <>
       <form className="FromButton">
         <input type="text" onChange={(event)=>handleInput(event.target.value)} value={inputValue}/>
         <button type="submit" onClick={(event)=>handleFormSubmit(event)}>add</button> 
       </form> 
-       <TodoList task={task}/>
+
+        <ol className="list">
+           { 
+                 task.map((item,index)=>{
+                 return <li key={index}>
+                    <p className={item.checked?"checked":"notChecked"}>{item.content}</p>
+                 <button className="checkBtn" onClick={()=>handleCheckBtn(item)}><PiChecksDuotone/></button>
+                 <button className="deleteBtn"onClick={()=>handleDeleteBtn(item)} ><MdDeleteForever/></button>
+                 </li>
+                    })
+           }
+        </ol>
       </>
     )
     
--- React.js by Thapa/vite-react/src/components/todoApp/todoList.jsx
+++ React.js by Thapa/vite-react/src/components/todoApp/todoList.jsx
@@ -1,18 +1,1 @@
-import { PiChecksDuotone } from "react-icons/pi";
-import { MdDeleteForever } from "react-icons/md";
-export const TodoList = ({task})=>{
 
-    return (
-        <ol className="list">
-           { 
-                 task.map((item,index)=>{
-                 return <li key={index}>
-                    <p>{item.content}</p>
-                 <button className="checkBtn"><PiChecksDuotone/></button>
-                 <button className="deleteBtn"><MdDeleteForever/></button>
-                 </li>
-                    })
-           }
-        </ol>
-    )
-}
