22b47fcec377792db046296fcea116859f0c470e
Shivam Kumar <shivam865889@gmail.com>
2025-08-19 23:32:10 +0530

    complition till form, list almost all without working of check and delete button

--- React.js by Thapa/vite-react/src/components/todoApp/form.jsx
+++ React.js by Thapa/vite-react/src/components/todoApp/form.jsx
@@ -1,18 +1,40 @@
-import {useState} from "react"
-export const FromButton = ()=>{
+import {useState} from "react";
+import {TodoList} from "./todoList"
+
+export const FormButton = ()=>{
+
   const [task,setTask]=useState([]);
+  const [inputValue, setInputValue]=useState("")
 
   const handleInput = (value)=>{
-           setTask(value);
+           setInputValue(value);
   }
  
-   
+  const handleFormSubmit = (event)=>{
+    event.preventDefault();
+    if(inputValue.trim()=="") return;
+
+    const boolArr = task.map((item)=>{
+      if(item.content == inputValue.trim())  
+        return true;
+      else
+        return false;
+    })
+    if(boolArr.includes(true)) return;
+
+    
+    setTask((prev)=>[...prev,{id:inputValue,content:inputValue,checked:false}])
+
+  }
 
     return (
+      <>
       <form className="FromButton">
-        <input type="text" onChange={(event)=>handleInput(event.target.value)} value={task}/>
-        <button type="submit">add</button>
-      </form>  
+        <input type="text" onChange={(event)=>handleInput(event.target.value)} value={inputValue}/>
+        <button type="submit" onClick={(event)=>handleFormSubmit(event)}>add</button> 
+      </form> 
+       <TodoList task={task}/>
+      </>
     )
     
 }
--- React.js by Thapa/vite-react/src/main.jsx
+++ React.js by Thapa/vite-react/src/main.jsx
@@ -4,13 +4,13 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.jsx';
 import "./components/todoApp/time.css"
 import {HeadingWithDate} from "./components/todoApp/time.jsx"
-import {FromButton} from "./components/todoApp/form.jsx"
+import {FormButton} from "./components/todoApp/form.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     <HeadingWithDate/>
-    <FromButton/>
+    <FormButton/>
   
   </StrictMode>,
 )
